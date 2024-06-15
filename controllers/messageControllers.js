f9586f90fcbec28aea56d67f489e4501ff0418b0
BakedPotato52 <kanakacharya52@gmail.com>
2024-06-15 12:35:37 +0530

    ..

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
-const Message = require("../models/messageModel");
-const User = require("../models/userModel");
+const Message = require("../models/messageModel").default;
+const User = require("../models/userModel").default;
 const Chat = require("../models/chatModel");
 
 //@description     Get all Messages
