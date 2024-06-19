bbd33a1d818023eeee0e97ab7123b93a7ccc0df9
BakedPotato52 <kanakacharya52@gmail.com>
2024-06-19 11:28:16 +0530

    changes

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chat-okt-1.onrender.com",
         credentials: true,
     },
 });
