247f4aa3c88ce1dab46638d84656f1b62c5a5c7a
BakedPotato52 <kanakacharya52@gmail.com>
2024-06-20 21:29:11 +0530

    ..

--- models/chatModel.js
+++ models/chatModel.js
@@ -4,6 +4,7 @@ const chatSchema = mongoose.Schema({
     chatName: { type: String, required: true },
     isGroupChat: { type: Boolean, default: false },
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 }, { timestamps: true });
 
 const Chat = mongoose.model("Chat", chatSchema);
