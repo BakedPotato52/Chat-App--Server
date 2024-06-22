122633a37f4b49086f8cf9dcbe695a765796acd0
BakedPotato52 <kanakacharya52@gmail.com>
2024-06-22 17:32:19 +0530

    c

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -6,7 +6,7 @@ const generateToken = require("../config/generateToken");
 // @route       POST /api/users
 // @access      Public
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body;
+    const { name, email, password, pic } = req.body;
 
     const userExists = await User.findOne({ email });
 
@@ -15,10 +15,12 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error("User already exists");
     }
 
+
     const user = await User.create({
         name,
         email,
         password,
+        pic: pic || "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
     });
 
     if (user) {
@@ -26,6 +28,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
+            pic: user.pic,
             token: generateToken(user._id),
         });
     } else {
@@ -47,6 +50,7 @@ const authUser = asyncHandler(async (req, res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
+            pic: user.pic,
             token: generateToken(user._id),
         });
     } else {
