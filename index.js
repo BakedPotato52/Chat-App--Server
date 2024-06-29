ada78ac6eb3a1060525660c842faafba4e9c338e
BakedPotato52 <kanakacharya52@gmail.com>
2024-06-29 23:00:37 +0530

    ..

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 app.use(cors({
-    origin: "https://main--chat-okt.netlify.app",
+    origin: "https://chat-okt.vercel.app",
     methods: ["GET", "POST"],
     credentials: true
 }));
